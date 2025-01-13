a7f4f508c5a27224376df58d0ebedc941e90e7ff
s85812 <s85812@bht-berlin.de>
2025-01-13 17:53:30 +0100

    Entityscan for main class

--- src/main/java/dev/ewd/mediashelf_spring/mediashelf_spring/MediashelfSpringApplication.java
+++ src/main/java/dev/ewd/mediashelf_spring/mediashelf_spring/MediashelfSpringApplication.java
@@ -2,8 +2,10 @@ package dev.ewd.mediashelf_spring.mediashelf_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "dev.ewd.mediashelf_spring.mediashelf_spring.model")
 public class MediashelfSpringApplication {
 
 	public static void main(String[] args) {
